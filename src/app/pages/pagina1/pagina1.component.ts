84385e38f47fa85201d0974d1ec469635b8d2804
angelxescomx08 <ah@xdevelop.mx>
2022-05-08 10:40:47 -0500

    todos los ciclos de vida

---
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,40 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styles: [
+  ]
+})
+export class Pagina1Component 
+  implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+              AfterViewInit, AfterViewChecked, OnDestroy {
+
+  constructor() { }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit Se llama cuando se construye el componente');
+  }
+
+}
